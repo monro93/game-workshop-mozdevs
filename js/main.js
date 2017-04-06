52e0fbcfa9c11a57c999582378867afbec9240dd
Albert Montolio <albertmonro@gmail.com>
2017-04-06 11:23:51 +0200

    Load and render the background

---
+++ js/main.js
@@ -0,0 +1,18 @@
+PlayState = {};
+
+//load game assets here
+PlayState.preload = function(){
+	this.game.load.image('background', 'images/background.png');
+};
+
+//create game entities and set upt wolrd here
+PlayState.create = function(){
+	this.game.add.image(0, 0, 'background');
+};
+
+//entry point
+window.onload = function(){
+	let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+	game.state.add('play', PlayState);
+	game.state.start('play');
+};
