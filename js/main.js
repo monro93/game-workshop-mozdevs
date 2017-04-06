f864150ee2723f3c4a9bd591b8f632c53995c9bd
Albert Montolio <albertmonro@gmail.com>
2017-04-06 11:50:00 +0200

    Create our main character

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 PlayState = {};
 
+function Hero(game, x, y){
+	Phaser.Sprite.call(this, game, x, y, 'hero');
+	this.anchor.set(0.5, 0.5);
+};
+
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
 //load game assets here
 PlayState.preload = function(){
 	this.game.load.json('level:1', 'data/level01.json');
@@ -11,6 +19,7 @@ PlayState.preload = function(){
 	this.game.load.image('grass:4x1', 'images/grass_4x1.png');
 	this.game.load.image('grass:2x1', 'images/grass_2x1.png');
 	this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+	this.game.load.image('hero', 'images/hero_stopped.png');
 };
 
 //create game entities and set upt wolrd here
@@ -22,11 +31,18 @@ PlayState.create = function(){
 PlayState._loadLevel = function(data){
 	//spawn all platforms
 	data.platforms.forEach(this._spawnPlatform, this);
-}
+	//spawn hero and enemies
+	this._spawnCharacters({hero: data.hero});
+};
 
 PlayState._spawnPlatform = function (platform){
 	this.game.add.sprite(platform.x, platform.y, platform.image);
-}
+};
+
+PlayState._spawnCharacters = function (data){
+	this.hero = new Hero(this.game, data.hero.x, data.hero.y);
+	this.game.add.existing(this.hero);
+};
 
 //entry point
 window.onload = function(){
